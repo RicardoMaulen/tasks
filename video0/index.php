99b1e41dd2485c8560c2eff750aa59716ebbc9a5
ricardomaulen <ricardomaulenlepe@gmail.com>
2023-12-08 12:05:31 +0100

    primer cambio

---
+++ video0/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>hola mundo</h1>
+  <p>jvh;k:j:l,,;::,:kn,;hbn,</p>
+  <span>hhhhh</span>
+</body>
+</html>
+
